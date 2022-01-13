5b4e2810d749b2f56fd2de43c6f0f47f7fd3312c
Inimesh <ijmeshoulam@gmail.com>
2022-01-13 12:37:53 +0000

    Adding thermostat temperature reset functionality

--- web.js
+++ web.js
@@ -20,6 +20,11 @@ app.post('/down', (req, res) => {
   res.send('Temperature decremented')
 })
 
+app.delete('/temperature', (req, res) => {
+  thermostat.reset();
+  res.send('Temperature reset')
+})
+
 
 console.log(`Listening on port: ${port}`);
 app.listen(port);
