3eadddc4b141ca3a32c98752ea04ed997f2d42d7
Inimesh <ijmeshoulam@gmail.com>
2022-01-16 11:00:36 +0000

    adding CORS rules and refactored server responses using JSON module methods

--- web.js
+++ web.js
@@ -1,30 +1,40 @@
 import express from "express";
+import cors from "cors";
 import { Thermostat } from './thermostat.js';
 
 const app = express();
-const port = 3000;
+app.use(
+  cors({
+    origin: "*"
+  })
+)
+const PORT = 3000;
 
 const thermostat = new Thermostat();
 
 app.get('/temperature', (req, res) => {
-  res.send(`Temperature: ${thermostat.getTemperature()}`);
+  const temp = thermostat.getTemperature()
+  res.send(JSON.stringify({"temperature": temp}));
 })
 
 app.post('/up', (req, res) => {
   thermostat.up();
-  res.send(`Temperature: ${thermostat.getTemperature()}`);
+  const temp = thermostat.getTemperature()
+  res.send(JSON.stringify({"temperature": temp}));
 });
 
 app.post('/down', (req, res) => {
   thermostat.down();
-  res.send(`Temperature: ${thermostat.getTemperature()}`);
+  const temp = thermostat.getTemperature()
+  res.send(JSON.stringify({"temperature": temp}));
 })
 
 app.delete('/temperature', (req, res) => {
   thermostat.reset();
-  res.send(`Temperature reset\nTemperature: ${thermostat.getTemperature()}`);
+  const temp = thermostat.getTemperature()
+  res.send(JSON.stringify({"temperature": temp}));
 })
 
 
-console.log(`Listening on port: ${port}`);
-app.listen(port);
+console.log(`Listening on port: ${PORT}`);
+app.listen(PORT);
